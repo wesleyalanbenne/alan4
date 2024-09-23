6dfa7f7f77ca9f2151222148abc0b69ea2284391
wesleyalanbenne <alan.bennemann@escola.pr.gov.br>
2024-09-23 10:16:32 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,41 @@
-function setup() {// funçao de configuraçao
-  createCanvas(600, 600);// tamanho da tela
-  background("white")// da tela 
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {//configuraçoes
+  createCanvas(1000, 1000);// tamanho do quadrado
+   background(color(100,0,0));// cor do quadrado
+  cor = color(random(0,255), random(0,255), random(0,255));//gerar cor aleatoria
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
 }
 
+
 function draw() {// desenhar
-  stroke("blue");// borda do retangulo
-  fill("red");// cor do preenchimento
+  
+  fill(cor);// cor do circulo
+  circle(posicaoHorizontal,posicaoVertical,50);
+ 
   
   
-  if (mouseIsPressed) {// pressione o mause 
-    rect(mouseX, mouseY, 20, 35);// seguir o mause 
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;//pro lado -1
   }
+  
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;// pro lado +1
+  }
+  
+  if (mouseY < posicaoVertical){// pra cima -1
+    posicaoVertical--;
+  }
+    
+  if (mouseY > posicaoVertical){// pra baixo +1
+    posicaoVertical++;
+  }
+  
+  if (mouseIsPressed){//quando preciona o mouse
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));// muda a cor do circulo quando pressionado
+  }
+    
 }
